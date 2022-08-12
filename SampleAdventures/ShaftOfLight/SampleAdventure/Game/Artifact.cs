8ceed4ba2690b3a62bfd3f9b079b338edce9d403
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-08-11 18:41:24 -0700

    Added SampleAdventure: ShaftOfLight.

---
+++ SampleAdventures/ShaftOfLight/SampleAdventure/Game/Artifact.cs
@@ -0,0 +1,30 @@
+﻿
+// Artifact.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using Eamon.Framework;
+using Eamon.Game.Attributes;
+using static SampleAdventure.Game.Plugin.PluginContext;
+
+namespace SampleAdventure.Game
+{
+	[ClassMappings]
+	public class Artifact : Eamon.Game.Artifact, IArtifact
+	{
+		public override string StateDesc
+		{
+			get
+			{
+				// Change shaft of light state description if no light source present
+
+				return Globals.EnableGameOverrides && Uid == 2 && IsInRoomUid(2) && gGameState != null && gGameState.Ls <= 0 ? "shimmering in the darkness" : base.StateDesc;
+			}
+
+			set
+			{
+				base.StateDesc = value;
+			}
+		}
+	}
+}
