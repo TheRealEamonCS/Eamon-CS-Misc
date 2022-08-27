fa1a9099dd2cc4a90ef29d98ebd83e6f1aa3d837
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-08-26 17:26:10 -0700

    Eamon/work branch aug 2022 (#1)
    * Deleted Samples\README.txt file.
    
    * Bug fix: in Samples Adventure, added a few more properties to PluginContext class.
    
    * Bug fix: for Samples Adventure, added missing EFFECTS.DAT file.
    
    * Bug fix: renamed Samples Adventure to Sample Adventure.
    
    * Added SampleAdventure: DustyFloor.
    
    * Added SampleAdventure: LightSwitch.
    
    * Added SampleAdventure: LightSwitch.
    
    * Bug fix: in Eamon-CS-Misc repository, renamed Samples folder to SampleAdventures.
    
    * Added SampleAdventure: RiverCurrent.
    
    * Added SampleAdventure: ShaftOfLight.
    
    * Bug fixes: in various Sample Adventures, repaired Room data.
    
    * Added SampleAdventure: Shotgun.
    
    * Added SampleAdventure: DeathTrap.
    
    * Added SampleAdventure: DropIntoOtherRoom.
    
    * Added some comments to various SampleAdventures.
    
    * More work on various Sample Adventures.
    
    * More work on various Sample Adventures.
    
    * More work on various Sample Adventures.
    
    * More work on various Sample Adventures.
    
    * More work on various Sample Adventures.
    
    * More work on various Sample Adventures.
    
    * Added SampleAdventure: InjuriousRoom.
    
    * Added SampleAdventure: Sinkhole.
    
    * Added SampleAdventure: ConditionalHint.
    
    * Added SampleAdventure: DisableCommands.
    
    * Bug fix: now using single-spacing by default for EamonRT.  Double-spacing is only available in custom adventures.  Refactored all existing custom games to maintain their current spacing behavior, so there should be no differences.  Standard adventures are always single-spaced.
    
    * Bug fix: now using single-spacing by default for EamonRT. Double-spacing is only available in custom adventures. Refactored all existing custom games to maintain their current spacing behavior, so there should be no differences. Standard adventures are always single-spaced.
    
    * Eamon CS Mobile 2.1.0 update for August 2022.

---
+++ SampleAdventures/InjuriousRoom/SampleAdventure/Game/Engine.cs
@@ -0,0 +1,43 @@
+﻿
+// Engine.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using Eamon.Framework;
+using Eamon.Game.Attributes;
+using static SampleAdventure.Game.Plugin.PluginContext;
+
+namespace SampleAdventure.Game
+{
+	[ClassMappings(typeof(IEngine))]
+	public class Engine : EamonRT.Game.Engine, EamonRT.Framework.IEngine
+	{
+		public override void InitArtifacts()
+		{
+			base.InitArtifacts();
+
+			// The @@001 token in Room 2 description will be replaced by a string returned from MacroFunc with key == 1
+
+			MacroFuncs.Add(1, () =>
+			{
+				var hazMatSuitArtifact = gADB[1];
+
+				Debug.Assert(hazMatSuitArtifact != null);
+
+				return !hazMatSuitArtifact.IsWornByCharacter() ? "And you have forgotten to wear your Haz-Mat suit!" : "";
+			});
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+				{ 1, new string[] { "haz mat suit", "suit" } },
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateArtifactSynonyms(synonym.Key, synonym.Value);
+			}
+		}
+	}
+}
---
+++ SampleAdventures/ShaftOfLight/SampleAdventure/Game/Room.cs
@@ -0,0 +1,43 @@
+﻿
+// Room.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Diagnostics;
+using Eamon.Framework;
+using Eamon.Framework.Primitive.Enums;
+using Eamon.Game.Attributes;
+using static SampleAdventure.Game.Plugin.PluginContext;
+
+namespace SampleAdventure.Game
+{
+	[ClassMappings]
+	public class Room : Eamon.Game.Room, IRoom
+	{
+		public override LightLevel LightLvl
+		{
+			get
+			{
+				// Computer Center always lit if shaft of light present
+
+				if (Globals.EnableGameOverrides && Uid == 2)
+				{
+					var shaftOfLightArtifact = gADB[2];
+
+					Debug.Assert(shaftOfLightArtifact != null);
+
+					return shaftOfLightArtifact.IsInRoom(this) ? LightLevel.Light : base.LightLvl;
+				}
+				else
+				{
+					return base.LightLvl;
+				}
+			}
+
+			set
+			{
+				base.LightLvl = value;
+			}
+		}
+	}
+}
