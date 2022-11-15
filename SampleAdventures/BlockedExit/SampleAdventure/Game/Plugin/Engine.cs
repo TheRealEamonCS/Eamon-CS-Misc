5bc294d30089553de5c85aff9148da23032fa291
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-11-14 19:35:03 -0800

    Added SampleAdventure: BlockedExit.

---
+++ SampleAdventures/BlockedExit/SampleAdventure/Game/Plugin/Engine.cs
@@ -0,0 +1,71 @@
+﻿
+// Engine.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using Eamon;
+using Eamon.Framework;
+using static SampleAdventure.Game.Plugin.Globals;
+
+namespace SampleAdventure.Game.Plugin
+{
+	public class Engine : EamonRT.Game.Plugin.Engine, Framework.Plugin.IEngine
+	{
+		public override RetCode LoadPluginClassMappings()
+		{
+			RetCode rc;
+
+			rc = base.LoadPluginClassMappings();
+
+			if (rc != RetCode.Success)
+			{
+				goto Cleanup;
+			}
+
+			rc = LoadPluginClassMappings01(Assembly.GetExecutingAssembly());
+
+		Cleanup:
+
+			return rc;
+		}
+
+		public override void PrintWhamHitObj(IArtifact artifact)
+		{
+			Debug.Assert(artifact != null);
+
+			// Tangle of overgrowth
+
+			if (artifact.Uid == 1)
+			{
+				// If not already "broken"
+
+				if (artifact.DoorGate.GetKeyUid() != -2)
+				{
+					gOut.Print("You hack your way into {0}, heading northeast.", artifact.GetTheName());
+				}
+			}
+			else
+			{
+				base.PrintWhamHitObj(artifact);
+			}
+		}
+
+		public override void InitArtifacts()
+		{
+			base.InitArtifacts();
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+				{ 1, new string[] { "vines", "tangle", "vine", "overgrowth", "path" } }
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateArtifactSynonyms(synonym.Key, synonym.Value);
+			}
+		}
+	}
+}
