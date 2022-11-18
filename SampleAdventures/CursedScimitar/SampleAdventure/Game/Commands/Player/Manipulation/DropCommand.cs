29a659e3bf9cca42c6139c263ee96e50a6907cc7
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-11-17 21:01:07 -0800

    Added SampleAdventure: CursedScimitar.

---
+++ SampleAdventures/CursedScimitar/SampleAdventure/Game/Commands/Player/Manipulation/DropCommand.cs
@@ -0,0 +1,38 @@
+﻿
+// DropCommand.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System;
+using System.Diagnostics;
+using Eamon.Framework;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Commands;
+using static SampleAdventure.Game.Plugin.Globals;
+
+namespace SampleAdventure.Game.Commands
+{
+	[ClassMappings]
+	public class DropCommand : EamonRT.Game.Commands.DropCommand, IDropCommand
+	{
+		public override void ProcessArtifact(IArtifact artifact)
+		{
+			Debug.Assert(artifact != null);
+
+			var amuletArtifact = gADB[3];
+
+			Debug.Assert(amuletArtifact != null);
+
+			// If scimitar is readied weapon and amulet not worn
+
+			if (artifact.Uid == 2 && ActorMonster.Weapon == 2 && !amuletArtifact.IsWornByCharacter())
+			{
+				gOut.Print("{0}The malevolent {1} stubbornly sticks to your hand!", DroppedArtifactList[0] != artifact ? Environment.NewLine : "", artifact.GetNoneName());
+			}
+			else
+			{
+				base.ProcessArtifact(artifact);
+			}
+		}
+	}
+}
---
+++ SampleAdventures/CursedScimitar/SampleAdventure/Game/Components/ComponentImpl.cs
@@ -0,0 +1,62 @@
+﻿
+// ComponentImpl.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System;
+using System.Diagnostics;
+using Eamon.Framework;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Components;
+using EamonRT.Framework.Primitive.Enums;
+using static SampleAdventure.Game.Plugin.Globals;
+
+namespace SampleAdventure.Game.Components
+{
+	[ClassMappings]
+	public class ComponentImpl : EamonRT.Game.Components.ComponentImpl, IComponentImpl
+	{
+		public override void PrintWeaponDropped(IRoom room, IMonster monster, IArtifact weapon, WeaponRevealType weaponRevealType)
+		{
+			Debug.Assert(room != null && monster != null && weapon != null);
+
+			var amuletArtifact = gADB[3];
+
+			Debug.Assert(amuletArtifact != null);
+
+			// If scimitar is readied weapon and amulet not worn
+
+			if (weapon.Uid == 2 && ((monster.IsCharacterMonster() && !amuletArtifact.IsWornByCharacter()) || (!monster.IsCharacterMonster() && !amuletArtifact.IsWornByMonster(monster))))
+			{
+				gOut.Write("{0}  {1} {2} {3}{4}!",
+					Environment.NewLine,
+					monster.IsCharacterMonster() ? "You" :
+					room.EvalLightLevel("The offender", monster.GetTheName(true, true, false, false, true)),
+					monster.IsCharacterMonster() ? "nearly drop" : "nearly drops",
+					monster.IsCharacterMonster() || room.IsLit() ?
+						(
+							(weaponRevealType == WeaponRevealType.Never ||
+							(weaponRevealType == WeaponRevealType.OnlyIfSeen && !weapon.Seen)) ?
+								weapon.GetArticleName() :
+								weapon.GetTheName()
+						) :
+						"a weapon",
+					monster.IsCharacterMonster() || room.IsLit() ?
+						string.Format
+						(
+							" but {0} to {1} hand",
+							weapon.EvalPlural("it stubbornly sticks", "they stubbornly stick"),
+							monster.IsCharacterMonster() ? "your" : monster.EvalGender("his", "her", "its")
+						) :
+						""
+					);
+
+				gEngine.ScimitarRecovered = true;
+			}
+			else
+			{
+				base.PrintWeaponDropped(room, monster, weapon, weaponRevealType);
+			}
+		}
+	}
+}
