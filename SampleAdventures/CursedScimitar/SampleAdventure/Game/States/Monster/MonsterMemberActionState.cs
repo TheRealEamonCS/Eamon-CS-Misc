ca5a535c0c31b33967bab8b0c4ba9fe00e96c52c
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-11-18 18:20:27 -0800

    Added SampleAdventure: CursedScimitar.

---
+++ SampleAdventures/CursedScimitar/SampleAdventure/Game/States/Monster/MonsterMemberActionState.cs
@@ -0,0 +1,34 @@
+﻿
+// MonsterMemberActionState.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Diagnostics;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.States;
+using static SampleAdventure.Game.Plugin.Globals;
+
+namespace SampleAdventure.Game.States
+{
+	[ClassMappings]
+	public class MonsterMemberActionState : EamonRT.Game.States.MonsterMemberActionState, IMonsterMemberActionState
+	{
+		public override void MonsterMemberMiscActionCheck02()
+		{
+			// base.MonsterMemberMiscActionCheck02();
+
+			var amuletArtifact = gADB[3];
+
+			Debug.Assert(amuletArtifact != null);
+
+			if (amuletArtifact.IsCarriedByMonster(LoopMonster))
+			{
+				gOut.Print("{0} wears {1}.", LoopMonster.GetTheName(true, true, false, false, true), amuletArtifact.GetArticleName());
+
+				amuletArtifact.SetWornByMonster(LoopMonster);
+
+				GotoCleanup = true;
+			}
+		}
+	}
+}
