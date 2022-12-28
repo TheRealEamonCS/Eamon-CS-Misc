ab35e60ed25ed08ad6d9c100f88f604e7732b367
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-12-27 18:02:28 -0800

    Eamon/work branch dec 2022 (#5)
    * Refactored RulesetVersion logic to standardize on the following:  0=EDX, 5=DDD 5, 6=DDD 6, 62=DDD 62, 7=DDD 7, 71=DDD 71. The Deep Canyon and The Vile Grimoire of Jaldi'al are both based on DDD5 (it would seem), so their RulesetVersions are changed. However, some added enhancements will ensure their behavior is identical to before. Eamon CS upgraded to version 2.2.0 due to ARTIFACTS.DAT change in The Deep Canyon; as always, the system will auto-upgrade any old game datafiles encountered (including saved games).
    
    * Refactored RulesetVersion logic to standardize on the following: 0=EDX, 5=DDD 5, 6=DDD 6, 62=DDD 62, 7=DDD 7, 71=DDD 71. The Deep Canyon and The Vile Grimoire of Jaldi'al are both based on DDD5 (it would seem), so their RulesetVersions are changed. However, some added enhancements will ensure their behavior is identical to before. Eamon CS upgraded to version 2.2.0 due to ARTIFACTS.DAT change in The Deep Canyon; as always, the system will auto-upgrade any old game datafiles encountered (including saved games).
    
    * Added SampleAdventure: AsciiArtMural.
    
    * Added SampleAdventure: AsciiArtMural.
    
    * Recompiled Eamon CS Mobile for merge of eamon/work-branch-dec-2022.

---
+++ SampleAdventures/AsciiArtMural/SampleAdventure/Game/Commands/Player/Manipulation/ExamineCommand.cs
@@ -0,0 +1,47 @@
+﻿
+// ExamineCommand.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System;
+using System.Diagnostics;
+using System.Text;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Commands;
+using EamonRT.Framework.Primitive.Enums;
+using static SampleAdventure.Game.Plugin.Globals;
+
+namespace SampleAdventure.Game.Commands
+{
+	[ClassMappings]
+	public class ExamineCommand : EamonRT.Game.Commands.ExamineCommand, IExamineCommand
+	{
+		public override void ProcessEvents(EventType eventType)
+		{
+			base.ProcessEvents(eventType);
+
+			// Mural - please see Engine.cs for more info on this (and credits)
+
+			if (eventType == EventType.AfterPrintArtifactFullDesc && DobjArtifact.Uid == 1)
+			{
+				gOut.Print("Gazing at the mural, you can't help but feel a sense of awe:");
+
+				gOut.Print("{0}", gEngine.LineSep);
+
+				gOut.Write("{0}Press any key to continue: ", Environment.NewLine);
+
+				gEngine.Buf.Clear();
+
+				var rc = gEngine.In.ReadField(gEngine.Buf, gEngine.BufSize02, null, ' ', '\0', true, null, gEngine.ModifyCharToNull, null, gEngine.IsCharAny);
+
+				Debug.Assert(gEngine.IsSuccess(rc));
+
+				gEngine.Thread.Sleep(150);
+
+				gOut.Print("{0}", gEngine.LineSep);
+
+				gOut.Print("{0}", Encoding.UTF8.GetString(Convert.FromBase64String(gEngine.MuralData)));
+			}
+		}
+	}
+}
