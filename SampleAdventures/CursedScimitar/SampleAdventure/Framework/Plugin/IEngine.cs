13b4478d599f44e70bdcb9092835d32b6057960a
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-03-14 17:10:01 -0700

    Bug fix: refactored CursedScimitar Sample Adventure to simplify it.

--- SampleAdventures/CursedScimitar/SampleAdventure/Framework/Plugin/IEngine.cs
+++ SampleAdventures/CursedScimitar/SampleAdventure/Framework/Plugin/IEngine.cs
@@ -3,10 +3,14 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using Eamon.Framework;
+
 namespace SampleAdventure.Framework.Plugin
 {
 	public interface IEngine : EamonRT.Framework.Plugin.IEngine
 	{
-		bool ScimitarRecovered { get; set; }
+		bool ShouldScimitarStickToHand(IMonster monster);
+
+		void PrintScimitarSticksToHand(IRoom room, IMonster monster, IArtifact artifact, bool combatFumble = false, bool newLinePrefix = false);
 	}
 }
--- SampleAdventures/CursedScimitar/SampleAdventure/Game/States/StateImpl.cs
+++ SampleAdventures/CursedScimitar/SampleAdventure/Game/States/StateImpl.cs
@@ -18,8 +18,8 @@ namespace SampleAdventure.Game.States
 		// This trick has never been demonstrated before in any game.  You can process all events
 		// system-wide in a single spot.  So if you have a bunch of Command classes in your game
 		// only created to process events, you can discard them and do all event processing right
-		// here.  In this example, we've avoided making game-specific PutCommand and ReadyCommand
-		// classes.
+		// here.  In this example, we've avoided making game-specific GiveCommand, PutCommand, and
+		// ReadyCommand classes.
 
 		public override void ProcessEvents(EventType eventType)
 		{
@@ -29,21 +29,13 @@ namespace SampleAdventure.Game.States
 
 			Debug.Assert(scimitarArtifact != null);
 
-			var amuletArtifact = gADB[3];
-
-			Debug.Assert(amuletArtifact != null);
-
 			// If scimitar is readied weapon and amulet not worn
 
-			if (eventType == EventType.BeforePutArtifact && State is IPutCommand pc && pc.DobjArtifact.Uid == 2 && pc.ActorMonster.Weapon == 2 && !amuletArtifact.IsWornByCharacter())
+			if ((eventType == EventType.AfterRefuseDeadBodyCheck && State is IGiveCommand gc && gc.DobjArtifact.Uid == 2 && gEngine.ShouldScimitarStickToHand(gc.ActorMonster)) || 
+					(eventType == EventType.BeforePutArtifact && State is IPutCommand pc && pc.DobjArtifact.Uid == 2 && gEngine.ShouldScimitarStickToHand(pc.ActorMonster)) ||
+					(eventType == EventType.BeforeReadyArtifact && State is IReadyCommand rc && rc.DobjArtifact.Uid != 2 && gEngine.ShouldScimitarStickToHand(rc.ActorMonster)))
 			{
-				gOut.Print("The malevolent {0} stubbornly sticks to your hand!", scimitarArtifact.GetNoneName());
-
-				GotoCleanup = true;
-			}
-			else if (eventType == EventType.BeforeReadyArtifact && State is IReadyCommand rc && rc.DobjArtifact.Uid != 2 && rc.ActorMonster.Weapon == 2 && !amuletArtifact.IsWornByCharacter())
-			{
-				gOut.Print("The malevolent {0} stubbornly sticks to your hand!", scimitarArtifact.GetNoneName());
+				gEngine.PrintScimitarSticksToHand(gCharRoom, gCharMonster, scimitarArtifact);
 
 				GotoCleanup = true;
 			}
