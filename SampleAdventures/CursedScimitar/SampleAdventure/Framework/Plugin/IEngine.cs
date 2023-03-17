8904d253b58be1b0f86ec5f2c5f54c188c9cb4c0
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-03-16 21:19:37 -0700

    Bug fix: refactored CursedScimitar Sample Adventure to simplify it.

--- SampleAdventures/CursedScimitar/SampleAdventure/Framework/Plugin/IEngine.cs
+++ SampleAdventures/CursedScimitar/SampleAdventure/Framework/Plugin/IEngine.cs
@@ -9,7 +9,7 @@ namespace SampleAdventure.Framework.Plugin
 {
 	public interface IEngine : EamonRT.Framework.Plugin.IEngine
 	{
-		bool ShouldScimitarStickToHand(IMonster monster);
+		bool ShouldScimitarStickToHand(IMonster monster);		// Note:  this should probably be a Monster method, but to keep it simple, we'll put it here
 
 		void PrintScimitarSticksToHand(IRoom room, IMonster monster, IArtifact artifact, bool combatFumble = false, bool newLinePrefix = false);
 	}
