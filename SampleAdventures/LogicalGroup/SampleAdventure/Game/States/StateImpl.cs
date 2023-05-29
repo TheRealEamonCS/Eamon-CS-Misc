b0c1311bb799af1581bb2b581f9932c6920f188c
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-05-29 16:38:20 -0700

    Bug fix: in LogicalGroup Sample Adventure, attempting to remove something other than knife, fork, or spoon from utensils results in regular error message.

--- SampleAdventures/LogicalGroup/SampleAdventure/Game/States/StateImpl.cs
+++ SampleAdventures/LogicalGroup/SampleAdventure/Game/States/StateImpl.cs
@@ -3,7 +3,9 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System;
 using Eamon.Game.Attributes;
+using Eamon.Game.Extensions;
 using EamonRT.Framework.Commands;
 using EamonRT.Framework.States;
 using static SampleAdventure.Game.Plugin.Globals;
@@ -15,9 +17,13 @@ namespace SampleAdventure.Game.States
 	{
 		public override void PrintDontFollowYou()
 		{
+			var utensilArtifactNames = new string[] { "knife", "fork", "spoon" };
+
+			var matches = gCommandParser?.DobjData?.Name != null ? utensilArtifactNames.FindAllIndexOf(uan => uan.StartsWith(gCommandParser.DobjData.Name, StringComparison.OrdinalIgnoreCase) || uan.EndsWith(gCommandParser.DobjData.Name, StringComparison.OrdinalIgnoreCase)) : new int[] { };
+
 			// Can't remove anything from the utensils
 
-			if (gCommandParser != null && gCommandParser.NextCommand is IRemoveCommand && gCommandParser.IobjArtifact != null && gCommandParser.IobjArtifact.Uid == 4)
+			if (gCommandParser != null && gCommandParser.NextCommand is IRemoveCommand && matches.Length > 0 && gCommandParser.IobjArtifact != null && gCommandParser.IobjArtifact.Uid == 4)
 			{
 				gOut.Print("You can manipulate the individual {0} directly.", gCommandParser.IobjArtifact.GetNoneName());
 			}
