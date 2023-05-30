65cbb99129cd8b15dce9c845d8c0d86f8d0a2d4e
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-05-29 23:33:00 -0700

    Bug fix: in LogicalGroup Sample Adventure, repaired corner-case bugs in PutCommand.

--- SampleAdventures/LogicalGroup/SampleAdventure/Game/Commands/Manipulation/PutCommand.cs
+++ SampleAdventures/LogicalGroup/SampleAdventure/Game/Commands/Manipulation/PutCommand.cs
@@ -4,6 +4,7 @@
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
 using System.Diagnostics;
+using System.Linq;
 using Eamon.Game.Attributes;
 using EamonRT.Framework.Commands;
 using EamonRT.Framework.States;
@@ -18,13 +19,28 @@ namespace SampleAdventure.Game.Commands
 		{
 			Debug.Assert(DobjArtifact != null && IobjArtifact != null);
 
+			var utensilArtifactUids = new long[] { 1, 2, 3 };
+
 			// Can't put anything into the utensils
 
 			if (IobjArtifact.Uid == 4)
 			{
-				gOut.Print("If you did that, it would no longer be a set of {0}.", IobjArtifact.GetNoneName());
+				if (utensilArtifactUids.Contains(DobjArtifact.Uid))
+				{
+					gOut.Print("That's already part of {0}.", IobjArtifact.GetTheName());
 
-				NextState = gEngine.CreateInstance<IMonsterStartState>();
+					NextState = gEngine.CreateInstance<IMonsterStartState>();
+				}
+				else if (DobjArtifact.Uid != 4)
+				{
+					gOut.Print("If you did that, it would no longer be a set of {0}.", IobjArtifact.GetNoneName());
+
+					NextState = gEngine.CreateInstance<IMonsterStartState>();
+				}
+				else
+				{
+					base.ExecuteForPlayer();
+				}
 			}
 			else
 			{
