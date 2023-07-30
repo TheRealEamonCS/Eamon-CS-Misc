83cdd346f53051c745e7a360c7b3c645ca0c2e77
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-07-29 21:06:39 -0700

    Bug fix: in InjuriousRoom Sample Adventure, hardened logic related to wearing/removing armor and Haz-Mat suit.

---
+++ SampleAdventures/InjuriousRoom/SampleAdventure/Game/Commands/Manipulation/RemoveCommand.cs
@@ -0,0 +1,39 @@
+﻿
+// RemoveCommand.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Diagnostics;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Commands;
+using EamonRT.Framework.States;
+using static SampleAdventure.Game.Plugin.Globals;
+
+namespace SampleAdventure.Game.Commands
+{
+	[ClassMappings]
+	public class RemoveCommand : EamonRT.Game.Commands.RemoveCommand, IRemoveCommand
+	{
+		public override void ExecuteForPlayer()
+		{
+			Debug.Assert(DobjArtifact != null);
+
+			var hazMatSuitArtifact = gADB[1];
+
+			Debug.Assert(hazMatSuitArtifact != null);
+
+			// Can't remove armor while wearing Haz-Mat suit
+
+			if (DobjArtifact.Uid == gGameState.Ar && hazMatSuitArtifact.IsWornByMonster(ActorMonster))
+			{
+				PrintWearingRemoveFirst01(hazMatSuitArtifact);
+
+				NextState = gEngine.CreateInstance<IStartState>();
+			}
+			else
+			{
+				base.ExecuteForPlayer();
+			}
+		}
+	}
+}
