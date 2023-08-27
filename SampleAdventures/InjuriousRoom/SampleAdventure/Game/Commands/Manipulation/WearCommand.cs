a399bd2068fd8ffba1e4aacfbb25a19f52504499
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-08-27 08:46:04 -0700

    Eamon/work branch aug 2023 (#13)
    * Bug fix: in InjuriousRoom Sample Adventure, hardened logic related to wearing/removing armor and Haz-Mat suit.
    
    * Bug fix: in InjuriousRoom Sample Adventure, hardened logic related to wearing/removing armor and Haz-Mat suit.
    
    * Bug fix: in InjuriousRoom Sample Adventure, hardened logic related to wearing/removing armor and Haz-Mat suit.
    
    * Recompiled Eamon CS Mobile for merge of eamon/work-branch-aug-2023.

---
+++ SampleAdventures/InjuriousRoom/SampleAdventure/Game/Commands/Manipulation/WearCommand.cs
@@ -0,0 +1,39 @@
+﻿
+// WearCommand.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Diagnostics;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Commands;
+using EamonRT.Framework.States;
+using static SampleAdventure.Game.Plugin.Globals;
+
+namespace SampleAdventure.Game.Commands
+{
+	[ClassMappings]
+	public class WearCommand : EamonRT.Game.Commands.WearCommand, IWearCommand
+	{
+		public override void ExecuteForPlayer()
+		{
+			Debug.Assert(DobjArtifact != null);
+
+			var hazMatSuitArtifact = gADB[1];
+
+			Debug.Assert(hazMatSuitArtifact != null);
+
+			// Can't wear armor while wearing Haz-Mat suit
+
+			if (DobjArtifact.Wearable != null && DobjArtifact.Wearable.Field1 > 1 && gGameState.Ar <= 0 && DobjArtifact.IsCarriedByMonster(ActorMonster) && hazMatSuitArtifact.IsWornByMonster(ActorMonster))
+			{
+				PrintWearingRemoveFirst01(hazMatSuitArtifact);
+
+				NextState = gEngine.CreateInstance<IStartState>();
+			}
+			else
+			{
+				base.ExecuteForPlayer();
+			}
+		}
+	}
+}
